ec30270863bba773454ea0a0bf79df38e351a019
James Clarke <james.clarke2@tudublin.ie>
2024-09-12 16:15:10 +0100

    Basic Mars Rotation

--- mars.gd
+++ mars.gd
@@ -1,13 +1,28 @@
 extends Node3D
 
+#Time taken in seconds for Mars to complete a full rotation
+#Should find more accurate value for this
+const FULL_ROT_SECS:float = 88560.0
+var startTime:float = 0.0
+var elapsedTime:float
+
+const MULTIPLIER = 2000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	startTime = Time.get_ticks_msec()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#pass
-	print("rotate")
-	rotate_y(0.01)
+	var elapsedRealSecs = (Time.get_ticks_msec() - startTime) / 1000.0
+	
+	var elapsedSimulatedSecs = elapsedRealSecs * MULTIPLIER
+	
+	var rotationSpeed =  (2*PI)/ FULL_ROT_SECS
+	
+	var angleToRotate = rotationSpeed * delta * MULTIPLIER
+	
+	rotate_y(angleToRotate)
+	
+	print("Full Rot Time = %f\n" % [FULL_ROT_SECS,])
+	print("ElapsedSimulatedSecs: %f\nElapsedRealSecs %f" % [elapsedSimulatedSecs,elapsedRealSecs] )

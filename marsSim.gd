c27d45cac89cccc35a27775365d1ba87a6c54b5d
James Clarke <james.clarke2@tudublin.ie>
2024-09-17 15:44:24 +0100

    Simple Orbit

--- marsSim.gd
+++ marsSim.gd
@@ -16,6 +16,8 @@ const MARS_RADIUS = REAL_MARS_RADIUS * modelScalar
 const PHOBOS_RADIUS:float = 9100.0 * modelScalar #Using polar radius for now
 const PHOBOS_SEMIMAJOR_AXIS = 937800 * modelScalar
 
+const PHOBOS_MOVE_SPEED_TEST = 0.1
+
 #Time Keeping
 var startTime:float = 0.0
 var elapsedRealSecs = 0
@@ -34,20 +36,35 @@ var phobos
 func _ready() -> void:
 	startTime = Time.get_ticks_msec()
 	
+	loadPhobos()
+
+func loadPhobos():
 	phobos = phobosScene.instantiate()
 	phobos.global_transform.origin = Vector3(PHOBOS_SEMIMAJOR_AXIS, 0, 0)	
 	phobos.setSize(PHOBOS_RADIUS)
 	add_child(phobos)	
-		
+	
 func _process(delta: float) -> void:
 	elapsedRealSecs += 1 * delta
 	elapsedSimulatedSecs += 1 * timeMultiplier * delta	
+		
 	rotateMars(delta)
-
+	movePhobos(delta)
+	
 func rotateMars(delta:float):
 	var rotationSpeed =  (2*PI)/ MARS_ROT_PERIOD	
 	var angleToRotate = rotationSpeed * delta * timeMultiplier	
 	planet.rotate_y(angleToRotate)
+
+var phobosOrbitAngle = 0.0
+
+func movePhobos(delta):	
+	phobosOrbitAngle += PHOBOS_MOVE_SPEED_TEST * delta
+	phobosOrbitAngle = fmod(phobosOrbitAngle, 2*PI)
+		
+	phobos.position.x = cos(phobosOrbitAngle) * PHOBOS_SEMIMAJOR_AXIS
+	phobos.position.z = sin(phobosOrbitAngle) * PHOBOS_SEMIMAJOR_AXIS
+	
 
 func increaseTime(value):
 	if ((timeMultiplier + (TIME_INCREMENT * (value/100))) <= MAX_TIME_MULT):

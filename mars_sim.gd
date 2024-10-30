7ed83b2aefe90d2c50786b3e0b932ba8207d36fe
James Clarke <james.clarke2@tudublin.ie>
2024-10-30 12:58:24 +0000

    Function to reset simulation

--- mars_sim.gd
+++ mars_sim.gd
@@ -13,14 +13,16 @@ Angle values are in degrees
 Mars has a radius of 0.5 in the model, so a scalar value is calculated,
 which is multilied by each "real" value to get the value to be used in the model
 """
-const REAL_MARS_RADIUS:float = 337620.0
-const MARS_RADIUS_IN_MODEL:float = 0.5
+const REAL_MARS_RADIUS: float = 337620.0
+const MARS_RADIUS_IN_MODEL: float = 0.5
 const MODEL_SCALER = MARS_RADIUS_IN_MODEL/REAL_MARS_RADIUS
 const MARS_ROT_PERIOD = 88642.44
 const MARS_RADIUS = REAL_MARS_RADIUS * MODEL_SCALER
+@onready var Mars = $Planet
+var _mars_initial_rot: Vector3
 
 # Phobos
-const PHOBOS_RADIUS:float = 9100.0 * MODEL_SCALER #Using polar radius for now
+const PHOBOS_RADIUS: float = 9100.0 * MODEL_SCALER #Using polar radius for now
 const PHOBOS_SEMIMAJOR_AXIS = 937800 * MODEL_SCALER
 const PHOBOS_ECCENTRICITY = 0.0151
 const PHOBOS_ORBIT_PERIOD = 27553.824
@@ -30,12 +32,14 @@ const PHOBOS_ORBIT_INCLINATION = 1.08
 const PHOBOS_SEMIMINOR_AXIS = PHOBOS_SEMIMAJOR_AXIS * sqrt(1-pow(PHOBOS_ECCENTRICITY, 2))
 const PhobosScn = preload("res://phobos.tscn")
 var _phobos
+var _phobos_initial_pos: Vector3
+var _phobos_initial_rot: Vector3
 var _phobos_orbit_angle = 0.0
-var _phobos_orbit_array:PackedVector3Array #Used for drawing orbit path for debugging
+var _phobos_orbit_array: PackedVector3Array #Used for drawing orbit path for debugging
 var _phobos_trail = []
 
 # Deimos
-const DEIMOS_RADIUS:float = 5100.0 * MODEL_SCALER #Using polar radius for now
+const DEIMOS_RADIUS: float = 5100.0 * MODEL_SCALER #Using polar radius for now
 const DEIMOS_SEMIMAJOR_AXIS = 2345900 * MODEL_SCALER
 const DEIMOS_ECCENTRICITY = 0.0005
 const DEIMOS_ORBIT_PERIOD = 109074.816
@@ -45,9 +49,11 @@ const DEIMOS_ORBIT_INCLINATION = 1.79
 const DEIMOS_SEMIMINOR_AXIS = DEIMOS_SEMIMAJOR_AXIS * sqrt(1-pow(DEIMOS_ECCENTRICITY, 2))
 const DeimosScn = preload("res://deimos.tscn")
 var _deimos
+var _deimos_initial_pos: Vector3
+var _deimos_initial_rot: Vector3 
 var _deimos_orbit_angle = 0.0
 var _deimos_orbit_plane
-var _deimos_orbit_array:PackedVector3Array #Used for drawing orbit path for debugging
+var _deimos_orbit_array: PackedVector3Array #Used for drawing orbit path for debugging
 var _deimos_trail = []
 
 # Time Keeping
@@ -62,7 +68,7 @@ var time_multiplier: float: # Externally a value between 0 and 100
 		var input = clamp(value, 0, 100)
 		_time_multiplier = remap(input, 0, 100, MIN_TIME_MULT, MAX_TIME_MULT)
 var _time_multiplier = 1
-var _start_time:float = 0.0
+var _start_time: float = 0.0
 
 # Trails
 const TrailObjectScn = preload("res://trail_object.tscn")
@@ -73,18 +79,40 @@ const TRAIL_LEN = 5
 		debug_mode = state
 		_toggle_debug_surfaces(state)		
 
-@onready var Mars = $Planet
+
 
 func _ready() -> void:
 	_start_time = Time.get_ticks_msec()	
+	
+	_mars_initial_rot = Mars.rotation
+	
 	_phobos = _instantiate_moon(PhobosScn, PHOBOS_RADIUS, PHOBOS_ORBIT_INCLINATION, PHOBOS_SEMIMAJOR_AXIS)
+	_phobos_initial_pos = _phobos.position
+	_phobos_initial_rot = _phobos.rotation
+	
 	_deimos = _instantiate_moon(DeimosScn, DEIMOS_RADIUS, DEIMOS_ORBIT_INCLINATION, DEIMOS_SEMIMAJOR_AXIS)
+	_deimos_initial_pos = _deimos.position
+	_deimos_initial_rot = _deimos.rotation
 	
 	
 func _process(delta: float) -> void:
 	_increase_time(delta)
 	_animate_sim(delta)
 	if debug_mode: _draw_debug_gizmos(delta)
+
+
+func reset_sim() -> void:
+	_time_multiplier = MIN_TIME_MULT
+	elapsed_real_secs = 0
+	elapsed_simulated_secs = 0
+	
+	Mars.rotation = _mars_initial_rot
+	
+	_phobos.position = _phobos_initial_pos
+	_phobos.rotation = _phobos_initial_rot
+	
+	_deimos.position = _deimos_initial_pos
+	_deimos_initial_rot = _deimos_initial_rot
 
 
 func get_real_time_mult() -> float:

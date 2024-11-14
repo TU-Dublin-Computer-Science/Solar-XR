0fd22a4d1201c8555992cded701f57466dc2b138
James Clarke <james.clarke2@tudublin.ie>
2024-11-14 17:25:01 +0000

    Node Component Changing When hovered/pressed

--- addons/mars-ui/content/ui/components/node/node.gd
+++ addons/mars-ui/content/ui/components/node/node.gd
@@ -1,6 +1,12 @@
 extends Container3D
 class_name UINode3D
 
+const NodeMaterial = preload("node.tres")
+
+const COLOR = Color(1.0, 1.0, 1.0, 0.3)
+const COLOR_HOVER = Color(0.5, 0.5, 0.5, 0.3)
+const COLOR_ACTIVE = Color(0.949, 0.353, 0.22, 0.3)
+
 signal on_node_down
 signal on_node_up
 
@@ -10,12 +16,15 @@ var active: bool = false:
 	set(value):
 		active = value
 		if !is_node_ready(): return
-		# TODO Handle selected visual changes on/off here
+		if active:
+			%Mesh.material_override.set_shader_parameter("color", COLOR_ACTIVE)
+		else:
+			%Mesh.material_override.set_shader_parameter("color", COLOR)
 	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	%Mesh.material_override = NodeMaterial.duplicate()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,7 +50,15 @@ func _on_press_up(event):
 	active = false
 	on_node_up.emit()
 	
-	
+func _on_ray_enter(_event: EventPointer):
+	if disabled:
+		return
+
+	%Mesh.material_override.set_shader_parameter("color", COLOR_HOVER)
+
+
+func _on_ray_leave(_event: EventPointer):
+	%Mesh.material_override.set_shader_parameter("color", COLOR)
 	
 	
 	

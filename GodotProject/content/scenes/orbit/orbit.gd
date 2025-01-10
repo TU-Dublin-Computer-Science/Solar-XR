e336facde9a3a206f1d6c3e826779de94a1486ab
James Clarke <james.clarke2@tudublin.ie>
2025-01-10 18:37:50 +0000

    Accurate Orbits

--- GodotProject/content/scenes/orbit/orbit.gd
+++ GodotProject/content/scenes/orbit/orbit.gd
@@ -1,8 +1,7 @@
 extends Node3D
 
 const OrbitMaterial = preload("res://content/assets/materials/orbit_material.tres")
-const ORBIT_MIN_THICKNESS = 0.002
-const ORBIT_VISUAL_OPACITY = 0.2 # Scale of 0-1 
+const ORBIT_POINTS: float = 512#1024
 
 const EPOCH_JULIAN_DATE = 2451545.0  # 2000-01-01.5
 
@@ -14,9 +13,10 @@ var julian_time: float:
 
 var _semimajor_axis: float
 var _eccentricity: float
-var _arg_of_periapsis: float
-var _mean_anomaly: float		# Radians
-var _inclination: float			# Radians
+var _arg_periapsis: float  # Radians
+var _mean_anomaly: float   # Radians
+var _inclination: float    # Radians
+var _lon_ascending_node: float  # Radians
 var _orbital_period: float
 
 var _body: Node3D
@@ -34,44 +34,104 @@ func _process(delta: float) -> void:
 func init(	p_body: Node3D, 
 			p_semimajor_axis: float,
 			p_eccentricity: float,
+			p_arg_periapsis: float,
 			p_mean_anomaly: float,
 			p_inclination: float, 
+			p_lon_ascending_node: float,
 			p_orbital_period: float,
 			p_julian_time: float,
 			p_model_scalar: float,
 			p_camera: XRCamera3D):
 
 	_body = p_body
-	_semimajor_axis = p_semimajor_axis
+	_semimajor_axis = p_semimajor_axis * p_model_scalar
 	_eccentricity = p_eccentricity
+	_arg_periapsis = deg_to_rad(p_arg_periapsis)
 	_mean_anomaly = deg_to_rad(p_mean_anomaly)
 	_inclination = deg_to_rad(p_inclination)
+	_lon_ascending_node = deg_to_rad(p_lon_ascending_node)
 	_orbital_period = p_orbital_period
 
 	julian_time = p_julian_time
 	_model_scalar = p_model_scalar	
 	_camera = p_camera
 	
-	rotate(Vector3.FORWARD, _inclination)
+	_orient_orbital_plane()
+	
+	_draw_orbit_visual()
 
-	add_child(_body)
-
-	_instantiate_orbit_visual()
+	%OrbitalPlane.add_child(_body)
 	
 	_update_body_position()
 	
 	_initialised = true
 	
 
+func _orient_orbital_plane(): 
+	%OrbitalPlane.transform.basis = Basis()  #Reset to initial orientation
+	
+	# Rotate orbital plane around the equatorial plane y axis (The Polar Axis)
+	%OrbitalPlane.rotate(Vector3(0,1,0), _lon_ascending_node)
+	
+	# Rotate orbital plane around the orbital plane x-axis (Line of Ascending Node)
+	%OrbitalPlane.rotate_object_local(Vector3(1, 0, 0), _inclination)
+	
+	# Rotate orbital plane around the orbital plane y-axis
+	%OrbitalPlane.rotate_object_local(Vector3(0, 1, 0), _arg_periapsis)
+
+
+func _draw_orbit_visual():
+	var orbit_mesh = ImmediateMesh.new()
+	
+	orbit_mesh.surface_begin(Mesh.PRIMITIVE_LINE_STRIP)
+	
+	var first_point: Vector3
+	
+	for i in range(ORBIT_POINTS):	
+		
+		var angle = (i / ORBIT_POINTS) * TAU
+		
+		orbit_mesh.surface_add_vertex(get_orbit_point(angle))
+
+	orbit_mesh.surface_add_vertex(get_orbit_point(0))  # Add first point to close loop
+	
+	orbit_mesh.surface_end()
+	
+	var orbit_visual = MeshInstance3D.new()
+	orbit_visual.mesh = orbit_mesh
+	%OrbitalPlane.add_child(orbit_visual)
+
+
+func get_orbit_point(angle: float):
+	# Note: Precalulate these two values if performance becomes issue
+	# Calculate the semi-minor axis based on eccentricity
+	var semiminor_axis = _semimajor_axis * sqrt(1 - _eccentricity * _eccentricity)
+	
+	# Calculate focal offset, which ensures central body remains at one focal point of the ellipse
+	var focal_offset = _semimajor_axis * _eccentricity
+	
+	var x = cos(angle) *  _semimajor_axis - focal_offset
+	var z = -sin(angle) * semiminor_axis
+
+	return Vector3(x, 0, z)
+
+
 func _update_body_position():
+	var true_anomaly = _get_true_anomaly()
+
+	_body.position = get_orbit_point(true_anomaly)
+
+
+func _get_true_anomaly():
 	var mean_motion = TAU/(_orbital_period * 86400)
 	
 	# 1. Get Current Mean anomaly 
 	# This is angle of body from periapsis (closest point to body) at the current time
 	var t = julian_time - EPOCH_JULIAN_DATE
+	
 	t *= 86400 #Convert days to seconds, as mean motion is rad/s
-	var current_mean_anomaly = _mean_anomaly + mean_motion * t
-	current_mean_anomaly = fmod(current_mean_anomaly, 360) # wrap to [0, 2π]
+	var current_mean_anomaly = _mean_anomaly + (mean_motion * t)
+	current_mean_anomaly = fmod(current_mean_anomaly, TAU) # wrap to [0, 2π]
 
 	# 2. Solve Kepler's equation for the eccentric anomaly
 	# This relates the current mean anomaly to orbit eccentricity
@@ -79,19 +139,8 @@ func _update_body_position():
 
 	# 3: Calculate the true anomaly (this is the actual value, not the mean)
 	var true_anomaly = 2 * atan(sqrt((1 + _eccentricity) / (1 - _eccentricity)) * tan(eccentric_anomaly / 2))	
-
-	# 4: Calculate the orbital radius
-	var orbital_radius = _semimajor_axis * (1 - _eccentricity ** 2) / (1 + _eccentricity * cos(true_anomaly))
 	
-	# 5: Scale down orbital radius for model
-	orbital_radius *= _model_scalar
-	
-	# 6: Compute the x, y position in the orbital plane
-	var x = orbital_radius * cos(true_anomaly)
-	var y = orbital_radius * sin(true_anomaly)
-	
-	_body.position = Vector3(x, 0, -y)
-
+	return true_anomaly
 
 # Solve Kepler's equation iteratively
 func _solve_keplers_equation(mean_anomaly, eccentricity):
@@ -103,26 +152,3 @@ func _solve_keplers_equation(mean_anomaly, eccentricity):
 			break
 		eccentric_anomaly -= delta / (1 - eccentricity * cos(eccentric_anomaly))
 	return eccentric_anomaly
-
-
-func _instantiate_orbit_visual():	
-	 # Formula for calculating semi-minor axis: b = a*sqrt(1-e^2)
-	var scaled_semimajor_axis = _semimajor_axis * _model_scalar
-	var scaled_semiminor_axis = scaled_semimajor_axis * sqrt(1-pow(_eccentricity, 2))
-	
-	var mesh_instance = MeshInstance3D.new()
-	var torus_mesh = TorusMesh.new()
-	
-	torus_mesh.inner_radius = scaled_semimajor_axis - ORBIT_MIN_THICKNESS
-	torus_mesh.outer_radius = scaled_semimajor_axis + ORBIT_MIN_THICKNESS
-	
-	mesh_instance.mesh = torus_mesh
-	mesh_instance.scale.z = scaled_semiminor_axis/scaled_semimajor_axis 
-	
-	var orbit_material = OrbitMaterial.duplicate()
-	var color = orbit_material.albedo_color
-	color.a = ORBIT_VISUAL_OPACITY
-	orbit_material.albedo_color = color
-	mesh_instance.material_override = orbit_material
-	
-	add_child(mesh_instance)
--- GodotProject/content/scenes/simulation/simulation.gd
+++ GodotProject/content/scenes/simulation/simulation.gd
@@ -88,7 +88,8 @@ func instantiate_simulation():
 		var satellite_data = _read_json_file(satellite_data_path)
 		
 		# This distance chosen for now as Neptune is this far away from Sun
-		if satellite_data["semimajor_axis"] < 4500000000: 
+		#if satellite_data["semimajor_axis"] < 4500000000: 
+		if satellite_data["semimajor_axis"] < 1000000: 
 		
 			var body = BodyScn.instantiate()
 			body.init(	satellite_data["name"],
@@ -105,8 +106,10 @@ func instantiate_simulation():
 			orbit.init(	body, 
 						satellite_data["semimajor_axis"],
 						satellite_data["eccentricity"], 
+						satellite_data["argument_periapsis"],
 						satellite_data["mean_anomaly"],
 						satellite_data["inclination"],
+						satellite_data["lon_ascending_node"],
 						satellite_data["orbital_period"], 
 						_unix_to_julian(time),
 						model_scalar,
@@ -118,7 +121,7 @@ func instantiate_simulation():
 
 func _unix_to_julian(unix_time: float):
 	var greg_date = Time.get_datetime_dict_from_unix_time(unix_time)
-	
+
 	var year = greg_date.year
 	var month = greg_date.month
 	var day = greg_date.day

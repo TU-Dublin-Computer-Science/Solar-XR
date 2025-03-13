a8ecce5624b54b9612f94333f966a5b88e6e0632
James Clarke <81041174+JamesClarke01@users.noreply.github.com>
2025-03-13 14:35:44 +0000

    Removed data files, improved DataGrabber.py functionality. (#16)
    * Creating satellite directory
    
    * Datascript CLI menu
    
    * Datagrabber now deletes old satellite files when regenerating
    
    * Removed duplicate data files
    
    ---------
    
    Co-authored-by: James Clarke <james.clarke2@tudublin.ie>

--- DataScript/DataGrabber.py
+++ DataScript/DataGrabber.py
@@ -1,9 +1,17 @@
+"""
+This script is for pulling the data from the downloaded html files in this directory and creating data files for each satellites,
+which are added to a "SatelliteDataFiles" subdirectory.
+Note that these won't include the info point data that has been added after.
+"""
+
 from bs4 import BeautifulSoup
 
-import json
+import json, os, shutil
+
+SATELLITE_DIR = "SatelliteDataFiles"
 
 def create_satellite_list() -> list:
-    satellite_physical_file = open("DataScript/Planetary Satellite Physical Parameters.html", "r")
+    satellite_physical_file = open("Planetary Satellite Physical Parameters.html", "r")
     satellite_phsyical_data = satellite_physical_file.read()
     satellite_physical_file.close()
 
@@ -23,7 +31,7 @@ def create_satellite_list() -> list:
             physical_satellite_data_list.append(data_object)
 
 
-    satellite_orbit_file = open("DataScript/Planetary Satellite Mean Elements.html", 'r')
+    satellite_orbit_file = open("Planetary Satellite Mean Elements.html", 'r')
     satellite_orbit_data = satellite_orbit_file.read()
     satellite_orbit_file.close()
 
@@ -67,12 +75,17 @@ def create_satellite_list() -> list:
     return satellite_data_list
             
 
-def create_satellite_files(satellite_list: list):
+def regen_satellite_files(satellite_list: list):
+
+    if os.path.exists(SATELLITE_DIR):
+        shutil.rmtree(SATELLITE_DIR)
+        os.makedirs(SATELLITE_DIR)
+    
 
     for satellite_data in satellite_list:
         json_string = json.dumps(satellite_data, indent=4)
 
-        file_path = "DataScript/SatelliteDataFiles/" + satellite_data["name"].lower() + ".json"
+        file_path = SATELLITE_DIR + "/" + satellite_data["name"].lower() + ".json"
 
         file = open(file_path, "w")
         file.write(json_string)
@@ -87,11 +100,38 @@ def print_planet_satellites(satellite_list: list, planet_name: str):
             print('"' + satellite_data["name"].lower() + '",')
             
 
-if __name__ == "__main__":
-    satellite_list = create_satellite_list()
-    
-    create_satellite_files(satellite_list)
-    
-    #print_planet_satellites(satellite_list, "uranus")
+def main_menu():
+    while True:
+        print("\n--- SolarXR Data File Script ---")
+        print("1. Generate Satellite Files")
+        print("2. Print a List of Satellites for a Specified Planet")
+        print("3. Exit")
 
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            # Ask for confirmation before regenerating satellite files
+            confirm = input(('Are you sure you want to generate the satellite files in a subdirectory "SatelliteDataFiles"?\n'
+                            "Any files in a subdirectory of the same name will be removed!\n"
+                            "(Press y to confirm):")).strip().lower()
+            if confirm == "y":
+                print("Generating satellite files...")
+                satellite_list = create_satellite_list()
+                regen_satellite_files(satellite_list)
+            else:
+                print("Operation canceled.")
+        elif choice == "2":
+            planet = input("Enter the name of the planet: ").strip().lower()
+            # Assuming we want to print the satellites of the given planet
+            satellite_list = create_satellite_list()
+            print_planet_satellites(satellite_list, planet)
+        elif choice == "3":
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+
+if __name__ == "__main__":
+    main_menu()
 

e11cdc1f54202c44420d702fb2f7eb3538dc81f7
James Clarke <james.clarke2@tudublin.ie>
2025-04-04 16:46:53 +0100

    Scene transition, but zoom incorrect

--- GodotProject/content/scenes/main/simulation.gd
+++ GodotProject/content/scenes/main/simulation.gd
@@ -45,6 +45,9 @@ var _new_focused_body: OrbitingBody
 var sun_data = Utils.load_json_file("res://content/data/bodies/sun.json")
 var _model_scalar
 
+var _focus_scene: OrbitingBody
+var _new_focus_scene: OrbitingBody
+
 var sim_time_scalar: float = DEFAULT_TIME_SCALAR:
 	set(value):
 		sim_time_scalar = value
@@ -70,7 +73,7 @@ var _sim_time: float:
 	set(value):
 		_sim_time = value
 		sim_time_changed.emit(_sim_time)
-		focus_scene.time = value
+		_focus_scene.time = value
 		
 		var sys_time = Time.get_unix_time_from_system()
 		
@@ -81,26 +84,26 @@ var _sim_time: float:
 var sim_scale: float:
 	set(value):
 		sim_scale = value
-		scale = Vector3(value, value, value)
+		_focus_scene.scale = Vector3(value, value, value)
 		
 		#Inverse scale applied to labels to keep them from being scaled with model
-		focus_scene.label_scale = 1 / sim_scale  
+		_focus_scene.label_scale = 1 / sim_scale  
 		
 		# Switch from showing planet orbit lines to showing planet moons once specific zoom threshold reached
 
 		var moon_show_thresh = (_focus_scale_body - (_focus_scale_body * 0.9))
 
-		focus_scene.satellite_orbits_visible = sim_scale <= moon_show_thresh # Hide/Show planet orbit lines
+		_focus_scene.satellite_orbits_visible = sim_scale <= moon_show_thresh # Hide/Show planet orbit lines
 		# If not the sun, show/hide satellites:
 		#_focused_body.satellites_visible = (_focused_body.ID == Mappings.planet_ID["sun"]) or (sim_scale > moon_show_thresh)
 		
 		sim_scale_changed.emit(_model_scalar * value)
 
-var focus_scene: OrbitingBody
+
 
 func _ready():
-	focus_scene = _create_focus_scene("sun")
-	add_child(focus_scene)
+	_focus_scene = _create_focus_scene("sun")
+	add_child(_focus_scene)
 #	_focused_body = %CentralBody
 	
 
@@ -113,9 +116,9 @@ func _process(delta: float) -> void:
 
 
 func setup():
-	focus_scene.satellites_visible = true
-	focus_scene.satellite_bodies_will_scale = true
-	focus_scene.visible = true
+	_focus_scene.satellites_visible = true
+	_focus_scene.satellite_bodies_will_scale = true
+	_focus_scene.visible = true
 
 
 func reset_state():
@@ -133,43 +136,26 @@ func init_time():
 func focus_body(p_new_focused_body_name: String):
 	"""This function sets up the transition to a focused body, which _handle_body_focusing() finishes"""
 	
-	#_new_focused_body = _get_body(p_new_focused_body_ID) # Set global var
+	_new_focus_scene = _create_focus_scene(p_new_focused_body_name)
 	
-#	if _new_focused_body != _focused_body:  # If new body being focused
-	var new_focus_scene = _create_focus_scene(p_new_focused_body_name)
+	# Get body to move to
+	for satellite in _focus_scene.satellites:
+		if satellite.body_name.to_lower() == p_new_focused_body_name.to_lower():
+			_new_focused_body = satellite
 	
-	add_child(new_focus_scene)
-	remove_child(focus_scene)
-	focus_scene = new_focus_scene
+	if _new_focused_body == null:
+		return
 	
-	focus_body_changed.emit(focus_scene)
-	
-	"""
 	_focus_scale_body =  0.5 / _new_focused_body.radius # Scale where body is visible
 
 	_focus_move_time_remaining = FOCUS_MOVE_TIME
+	_focus_zoom_in_speed = abs(_focus_scale_body - FOCUS_SCALE_BIRDS_EYE) / FOCUS_ZOOM_TIME
 	
-	if _new_focused_body == _focused_body:  #If same body being focused
-		_focused_body = _new_focused_body
+	_focus_zoom_out_target = FOCUS_SCALE_BIRDS_EYE # Target is the space birds eye veiw
+	_focus_zoom_out_speed = abs(_focus_zoom_out_target - sim_scale) / FOCUS_ZOOM_TIME
+	
+	_focus_state = FocusState.ZOOM_OUT  
 
-		if sim_scale >= _focus_scale_body: # If need to zoom out
-			_focus_zoom_out_target = _focus_scale_body  # Zoom out target is the planet view
-
-			_focus_zoom_out_speed = abs(_focus_zoom_out_target - sim_scale) / FOCUS_ZOOM_TIME
-			_focus_state = FocusState.ZOOM_OUT
-		else: # If need to zoom in
-			_focus_zoom_in_speed = abs(_focus_scale_body - sim_scale) / FOCUS_ZOOM_TIME
-			_focus_state = FocusState.ZOOM_IN
-	else: # If new body being focused
-		_focus_zoom_in_speed = abs(_focus_scale_body - FOCUS_SCALE_BIRDS_EYE) / FOCUS_ZOOM_TIME
-		if sim_scale >= FOCUS_SCALE_BIRDS_EYE: # If need to zoom out
-			_focus_zoom_out_target = FOCUS_SCALE_BIRDS_EYE # Target is the space birds eye veiw
-			_focus_zoom_out_speed = abs(_focus_zoom_out_target - sim_scale) / FOCUS_ZOOM_TIME
-			_focus_state = FocusState.ZOOM_OUT  
-		else:
-			_focused_body = _new_focused_body
-			_focus_state = FocusState.MOVE  # Else move to new body
-	"""
 
 func _handle_body_focusing(delta: float):
 	match(_focus_state):
@@ -185,25 +171,33 @@ func _handle_body_focusing(delta: float):
 		FocusState.MOVE:
 			_focus_move_time_remaining -= delta
 			var body_position: Vector3 = %Simulation.to_local(_focused_body.body.global_position)
-			var focus_sim_move_target = %CentralBody.position - body_position  
+			var focus_sim_move_target = _focus_scene.position - body_position  
 			var focus_sim_move_dir = -body_position.normalized()
-			var _focus_sim_move_speed = (focus_sim_move_target - %CentralBody.position).length() / _focus_move_time_remaining
+			var _focus_sim_move_speed = (focus_sim_move_target - _focus_scene.position).length() / _focus_move_time_remaining
 		
 			var step = focus_sim_move_dir * _focus_sim_move_speed * delta
 			# Check if at target, accounting for overshooting
-			if step.length() >= %CentralBody.position.distance_to(focus_sim_move_target):
-				%CentralBody.position = focus_sim_move_target
+			if step.length() >= _focus_scene.position.distance_to(focus_sim_move_target):
+				_focus_scene.position = focus_sim_move_target
 				
 				_focus_action_after_wait = FocusState.ZOOM_IN
 				_focus_wait_timer = 0
 				_focus_state = FocusState.WAIT
 			else:
-				%CentralBody.position += step
+				_focus_scene.position += step
 		FocusState.ZOOM_IN:
 			if sim_scale >= _focus_scale_body:
 				sim_scale = _focus_scale_body
 				
 				_focus_state = FocusState.FOCUSED
+				
+				add_child(_new_focus_scene)
+	
+				remove_child(_focus_scene)
+				_focus_scene = _new_focus_scene
+	
+				focus_body_changed.emit(_focus_scene)
+				
 			else:
 				sim_scale += _focus_zoom_in_speed * delta
 		FocusState.WAIT:
@@ -211,10 +205,6 @@ func _handle_body_focusing(delta: float):
 				_focus_state = _focus_action_after_wait
 			else:
 				_focus_wait_timer += delta
-
-	if _focused_body and _focus_state != FocusState.MOVE and _focus_action_after_wait != FocusState.MOVE: # Keep body in focus
-		var body_position: Vector3 = %Simulation.to_local(_focused_body.body.global_position)
-		%CentralBody.position =  %CentralBody.position - body_position  
 
 
 func _create_focus_scene(body_name: String) -> OrbitingBody:

563c8be0c6f3dca591c5426f91d63da207154a71
James Clarke <james.clarke2@tudublin.ie>
2025-05-01 12:34:21 +0100

    Restored button cooldown

--- GodotProject/content/scenes/menu/menu_touch_info.gd
+++ GodotProject/content/scenes/menu/menu_touch_info.gd
@@ -3,6 +3,6 @@ extends Node3D
 func _enter_tree() -> void:
 	# Wait before button is activated to prevent user proceedding immediately
 	$InfoNode.disabled = true
-	#await get_tree().create_timer(2.0).timeout
+	await get_tree().create_timer(2.0).timeout
 	$InfoNode.disabled = false
 	

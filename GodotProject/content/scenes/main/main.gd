dfcd414b3a9392ec58d418ce85e008091b8c9693
James Clarke <james.clarke2@tudublin.ie>
2025-10-07 15:22:56 +0100

    Info node screen deactivating on body switch

--- GodotProject/content/scenes/main/main.gd
+++ GodotProject/content/scenes/main/main.gd
@@ -211,6 +211,7 @@ func _focus_parent():
 	
 	_connect_info_nodes(_focus_scene.focused_body)
 	_update_body_menu()
+	%InfoNodeScreen.deactivate()
 
 
 func _focus_child(body_name: String):
@@ -224,6 +225,8 @@ func _focus_child(body_name: String):
 	
 	_focus_scene.focus_animation_finished.connect(_focus_child_animation_finished)
 	_focus_scene.start_focus_animation(body_name)
+	
+	%InfoNodeScreen.deactivate()
 
 
 func _focus_child_animation_finished():

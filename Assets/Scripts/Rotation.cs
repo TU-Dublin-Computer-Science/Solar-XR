f46ba99eb9f5b065a28412de44be1f2a57a6e930
JamesClarke01 <jamesclarkedublin@gmail.com>
2025-10-24 13:39:14 +0100

    Rotating Sun

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Vector3 rotationSpeed = new Vector3(0f, 50f, 0f);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
+}

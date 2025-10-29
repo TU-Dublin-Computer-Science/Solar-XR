d6819c971ac66aa4a28d51c7953c2c2c321a8e75
JamesClarke01 <jamesclarkedublin@gmail.com>
2025-10-29 12:03:51 +0000

    Meta VR SDK set up

---
+++ SolarXRUnity_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,57 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void Rotation_Start_m1BA3811BE9CEEDA009865CE62B8679DE8DA4DCD7 (void);
+extern void Rotation_Update_m54B806F62ACA173B14FC003BCD36B32D5DBDC118 (void);
+extern void Rotation__ctor_mC51A8B2D3741FB75120E1D3690D504031D4D77D6 (void);
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	Rotation_Start_m1BA3811BE9CEEDA009865CE62B8679DE8DA4DCD7,
+	Rotation_Update_m54B806F62ACA173B14FC003BCD36B32D5DBDC118,
+	Rotation__ctor_mC51A8B2D3741FB75120E1D3690D504031D4D77D6,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	18946,
+	18946,
+	18946,
+	18946,
+	18946,
+	31656,
+	18946,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
